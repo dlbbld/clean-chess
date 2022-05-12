e95873cc24f91daf2aed6cff5f71e5cea4a17d11
danie <danie@TpPrimary>
2022-05-12 21:21:42 +0200

    analyzer
    removed ouput parametrization which will never be needed

--- src/main/java/com/dlb/chess/analysis/print/AnalyzerPrint.java
+++ src/main/java/com/dlb/chess/analysis/print/AnalyzerPrint.java
@@ -5,12 +5,9 @@ import java.util.List;
 
 import org.eclipse.jdt.annotation.NonNull;
 
-import com.dlb.chess.analysis.Analyzer;
-import com.dlb.chess.analysis.model.Analysis;
 import com.dlb.chess.analysis.model.YawnHalfMove;
 import com.dlb.chess.common.NonNullWrapperCommon;
 import com.dlb.chess.common.constants.ChessConstants;
-import com.dlb.chess.common.enums.DeadPosition;
 import com.dlb.chess.common.enums.EnPassantCaptureRuleThreefold;
 import com.dlb.chess.common.interfaces.ApiBoard;
 import com.dlb.chess.common.model.ClaimAhead;
@@ -21,7 +18,6 @@ import com.dlb.chess.common.utility.ThreefoldClaimAheadUtility;
 import com.dlb.chess.common.utility.YawnMoveUtility;
 import com.dlb.chess.internationalization.Message;
 import com.dlb.chess.pgn.reader.PgnReader;
-import com.dlb.chess.pgn.reader.enums.ResultTagValue;
 import com.dlb.chess.pgn.reader.model.PgnFile;
 
 //for class organization only, keep methods protected as already used as delegate in main class Analysis
@@ -29,10 +25,6 @@ public class AnalyzerPrint {
 
   private static final int REPETITION_COUNT_THRESHOLD = ChessConstants.THREEFOLD_REPETITION_RULE_THRESHOLD;
   private static final int YAWN_FULL_MOVE_COUNT_THRESHOLD = 25;
-
-  private static final boolean IS_PRINT_THREEFOLD_YES_NO = false;
-  private static final boolean IS_PRINT_SEVENTY_FIVE_MOVE_RULE_YES_NO = false;
-  private static final boolean IS_PRINT_RESULT = false;
 
   protected static void printAnalysis(String pgn) throws Exception {
 
@@ -51,8 +43,6 @@ public class AnalyzerPrint {
   }
 
   protected static void printAnalysis(ApiBoard board) throws Exception {
-    final Analysis analysis = Analyzer.calculateAnalysis(board);
-
     final @NonNull List<String> output = new ArrayList<>();
 
     // repetition
@@ -76,22 +66,6 @@ public class AnalyzerPrint {
       output.add(listChronic);
     }
 
-    if (IS_PRINT_THREEFOLD_YES_NO) {
-      addMainSection(output, "analysis.repetition.threefold.yesno.title");
-      if (analysis.hasThreefoldRepetition()) {
-        output.add(Message.getString("analysis.repetition.threefold.yesno.yes"));
-      } else {
-        output.add(Message.getString("analysis.repetition.threefold.yesno.no"));
-      }
-
-      addMainSection(output, "analysis.repetition.fivefold.yesno.title");
-      if (analysis.hasFivefoldRepetition()) {
-        output.add(Message.getString("analysis.repetition.fivefold.yesno.yes"));
-      } else {
-        output.add(Message.getString("analysis.repetition.fivefold.yesno.no"));
-      }
-    }
-
     // yawn move
     final List<List<YawnHalfMove>> yawnMoveListList = YawnMoveUtility.calculateYawnMoveRule(board,
         2 * YAWN_FULL_MOVE_COUNT_THRESHOLD);
@@ -105,68 +79,26 @@ public class AnalyzerPrint {
     }
 
     addMainSection(output, "analysis.yawnmove.fiftyMoves.title");
-    if (analysis.hasFiftyMoveRule()) {
+    final var hasFiftyMoveRule = calculateHasFiftyMoveRule(yawnMoveListList);
+    if (hasFiftyMoveRule) {
       output.add(Message.getString("analysis.yawnmove.fiftyMoves.yes"));
     } else {
       output.add(Message.getString("analysis.yawnmove.fiftyMoves.no"));
     }
 
-    if (IS_PRINT_SEVENTY_FIVE_MOVE_RULE_YES_NO) {
-      addMainSection(output, "analysis.yawnmove.seventyFiveMoves.title");
-      if (analysis.hasSeventyFiveMoveRule()) {
-        output.add(Message.getString("analysis.yawnmove.seventyFiveMoves.yes"));
-      } else {
-        output.add(Message.getString("analysis.yawnmove.seventyFiveMoves.no"));
-      }
-    }
-
-    if (IS_PRINT_RESULT) {
-      // board result
-      addMainSection(output, "analysis.board.result");
-
-      final var boardResult = new StringBuilder();
-      if (board.isCheckmate()) {
-        switch (board.getHavingMove()) {
-          case BLACK:
-            boardResult.append(Message.getString("analysis.board.checkmatebyWhite"));
-            appendPoints(boardResult, ResultTagValue.WHITE_WON);
-            break;
-          case WHITE:
-            boardResult.append(Message.getString("analysis.board.checkmatebyBlack"));
-            appendPoints(boardResult, ResultTagValue.BLACK_WON);
-            break;
-          case NONE:
-          default:
-            throw new IllegalArgumentException();
-        }
-      } else if (board.isStalemate()) {
-        boardResult.append(Message.getString("analysis.board.stalemate"));
-        appendPoints(boardResult, ResultTagValue.DRAW);
-      } else if (board.isFivefoldRepetition()) {
-        boardResult.append(Message.getString("analysis.board.fivefoldRepetition"));
-        appendPoints(boardResult, ResultTagValue.DRAW);
-      } else if (board.isSeventyFiftyMove()) {
-        boardResult.append(Message.getString("analysis.board.seventyFiveMoves"));
-        appendPoints(boardResult, ResultTagValue.DRAW);
-      } else if (board.isInsufficientMaterial()) {
-        boardResult.append(Message.getString("analysis.board.insufficientMaterial"));
-        appendPoints(boardResult, ResultTagValue.DRAW);
-      } else if (board.isDeadPosition() == DeadPosition.YES) {
-        boardResult.append(Message.getString("analysis.board.deadPosition"));
-        appendPoints(boardResult, ResultTagValue.DRAW);
-      } else {
-        boardResult.append(Message.getString("analysis.board.ongoing"));
-      }
-      output.add(NonNullWrapperCommon.toString(boardResult));
-    }
-
     printList(output);
   }
 
-  private static void appendPoints(StringBuilder result, ResultTagValue resultTagValue) {
-    result.append(" (");
-    result.append(resultTagValue.getValue());
-    result.append(")");
+  private static boolean calculateHasFiftyMoveRule(List<List<YawnHalfMove>> yawnMoveListList) {
+    // add main section
+    for (final List<YawnHalfMove> yawnMoveList : yawnMoveListList) {
+      for (final YawnHalfMove yawnHalfMove : yawnMoveList) {
+        if (yawnHalfMove.sequenceLength() >= ChessConstants.FIFTY_MOVE_RULE_HALF_MOVE_CLOCK_THRESHOLD) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
   private static void addFirstMainSection(List<String> output, String key) {
