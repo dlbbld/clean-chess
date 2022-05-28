bc70b353827d279b161a096bc3581a32d08037f7
danie <danie@TpSecondary>
2022-05-28 15:31:13 +0200

    test

--- src/test/java/com/dlb/chess/test/lichess/pgn/LichessCheckMultiplePgn.java
+++ src/test/java/com/dlb/chess/test/lichess/pgn/LichessCheckMultiplePgn.java
@@ -58,6 +58,7 @@ public class LichessCheckMultiplePgn extends AbstractLichessCheck {
 
     // test commit
     // test
+    // test
 
     try (final Scanner myReader = new Scanner(file, StandardCharsets.ISO_8859_1);) {
       while (myReader.hasNextLine()) {
