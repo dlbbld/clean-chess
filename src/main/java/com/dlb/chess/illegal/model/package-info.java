65f4df5da4097821239bc73e6f922097b0b1b9b9
danie <danie@host.docker.internal>
2023-03-27 22:02:31 +0200

    SonarLint
    moved package from clean-chess to belonging speaking-chessboard

--- src/main/java/com/dlb/chess/illegal/model/package-info.java
+++
@@ -1,4 +0,0 @@
-@NonNullByDefault
-package com.dlb.chess.illegal.model;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
--- src/test/java/com/dlb/chess/test/illegal/package-info.java
+++
@@ -1,4 +0,0 @@
-@NonNullByDefault
-package com.dlb.chess.test.illegal;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
