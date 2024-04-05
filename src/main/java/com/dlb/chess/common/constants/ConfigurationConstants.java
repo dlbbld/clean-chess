6d0d28a15283406da8e256f26647f2e8245e7dce
dbaechli <dbaechli@ZO-15442.d.ethz.ch>
2024-04-05 12:37:01 +0200

    removed hardcoding of project path

--- src/main/java/com/dlb/chess/common/constants/ConfigurationConstants.java
+++ src/main/java/com/dlb/chess/common/constants/ConfigurationConstants.java
@@ -2,13 +2,15 @@ package com.dlb.chess.common.constants;
 
 import java.util.Locale;
 
+import com.dlb.chess.common.utility.BasicUtility;
+
 public abstract class ConfigurationConstants {
 
   // when using the speaking chess board, the system variable GOOGLE_APPLICATION_CREDENTIALS must be set to the path of
   // the json file for the google text to speech service account (non free service)
 
   public static final String TEMP_FOLDER_PATH = "C:\\temp";
-  public static final String PROJECT_ROOT_FOLDER_PATH = "C:\\Users\\danie\\git\\clean-chess";
+  public static final String PROJECT_ROOT_FOLDER_PATH = BasicUtility.readProjectFolderPath();
 
   public static final int DGT_MY_BLUETOOTH_BOARD_ID = 23944;
   public static final int DGT_MY_USB_BOARD_ID = 43462;
