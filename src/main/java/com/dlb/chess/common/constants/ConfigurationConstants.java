7f3269afd616ac33d81a41a49029dc40c8742395
Daniel Bächli <62600114+dlbbld@users.noreply.github.com>
2026-05-09 09:42:31 +0200

    Remove all DGT-derived material - follow-up

--- src/main/java/com/dlb/chess/common/constants/ConfigurationConstants.java
+++ src/main/java/com/dlb/chess/common/constants/ConfigurationConstants.java
@@ -8,9 +8,6 @@ import com.dlb.chess.common.utility.BasicUtility;
 
 public abstract class ConfigurationConstants {
 
-  // when using the speaking chess board, the system variable GOOGLE_APPLICATION_CREDENTIALS must be set to the path of
-  // the json file for the google text to speech service account (non free service)
-
   @SuppressWarnings("null")
   public static final Path TEMP_FOLDER_PATH = Paths.get(System.getProperty("java.io.tmpdir"));
   public static final Path PROJECT_ROOT_FOLDER_PATH = BasicUtility.readProjectFolderPath();
